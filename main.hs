84a1b0dc1006e60525f8377e64e5d56618574217
Artyom Voronin <artyom.voronin@firma.seznam.cz>
2025-11-14 20:37:04 +0100

    init; add blueprint

---
+++ main.hs
@@ -0,0 +1,17 @@
+import Html
+
+main :: IO ()
+main = putStrLn (render myhtml)
+
+myhtml :: Html
+myhtml = 
+    html_ 
+        "Page title<>"
+        ( append_ 
+            (h1_ "Welcome!")
+            ( append_ 
+                (p_ "This is a simple HTML page generated in Haskell.")
+                (p_ "Enjoy coding!")
+            )
+        )
+
